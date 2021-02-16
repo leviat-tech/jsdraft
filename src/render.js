d9624234d3a42e2c6520d77fe88e8a4700cdc8ec
Stephen <squebe@crh.io>
2021-02-16 12:38:22 -0700

    Update render to reflect new draft spec and move into src

--- src/render.js
+++ src/render.js
@@ -1,31 +1,33 @@
 #!/usr/bin/env node
 const { program } = require('commander');
-const Composition = require('./src/composition.js');
+const path = require('path');
+const load = require('./loaders/load');
+const Sketch = require('./sketch/sketch.js');
 
 
-function main(renderer, path, args) {
+function main(renderer, feature, args) {
   // parse parameters
   const params = args.map(a => JSON.parse(a));
 
   // import desired render function
-  const render = require(`./src/render/${renderer}.js`);
+  const render = require(`./renderers/${renderer}.js`);
 
   // import root draft function
-  const draft = require(path);
+  const draft = load(path.join(process.cwd(), feature))
 
-  // create blank composition to pass to root draft function
-  const blank = new Composition();
+  // create blank sketch to pass to root draft function
+  const blank = new Sketch();
 
   // execute root draft function and render
-  const composition = draft(blank, ...args);
-  render(composition);
+  const sketch = draft(blank, ...args);
+  render(sketch);
 }
 
 program
   .arguments('<renderer> <draft> [arguments...]')
   .description('renderer', {
     renderer: 'render function',
-    draft: 'root draft function to generate composition',
+    draft: 'root draft function to generate sketch',
     args: 'json arguments to pass root draft function'
   })
   .action(main);
