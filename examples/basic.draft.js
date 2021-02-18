57fe6c82043fabc3afd1b0b51aa8182ede8ce88d
Stephen <squebe@crh.io>
2021-02-18 12:20:53 -0700

    Fix basic sketch example

--- examples/basic.draft.js
+++ examples/basic.draft.js
@@ -1,28 +1,8 @@
 module.exports = function basic(sketch) {
-  // sketch.node.name = 'a';
-  // a = sketch;
-  // b = sketch.create({name: 'b'})
-  // c = sketch.create({name: 'c'})
-  // d = sketch.create({name: 'd'})
-  // e = sketch.create({name: 'e'})
-  // f = sketch.create({name: 'f'})
-  // g = sketch.create({name: 'g'})
-  // h = sketch.create({name: 'h'})
-  //
-  //
-  // a.add(b)
-  //   b.add(e)
-  //     e.add(g)
-  //       e.add(h)
-  //
-  // a.add(c)
-  //   b.add(f)
-  //
-  // a.add(d)
-  //
-  //
-  // const result = sketch.find(s => s.node.name == 'h');
-  // console.log(result)
-  // return sketch;
-  return sketch.new.golden(10);
+  return sketch.draw(
+    sketch.circle(50, 50, 10),
+    sketch.circle(25, 25, 10)
+  )
+  .fill('blue')
+  .stroke(2, 'red');
 };
