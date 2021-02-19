93ff3909ba10da59d828b393a3098bb89cbefc1d
Stephen <squebe@crh.io>
2021-02-19 14:44:54 -0700

    Remove polyline segments, this has been renamed to polycurve

--- src/features/geometric/polyline-segments.js
+++
@@ -1,8 +0,0 @@
-const Polyline = require('../../entities/geometric/polyline.js');
-
-
-module.exports = function polyline_segments(sketch, ...args) {
-  return sketch.create({
-    entities: [new Polyline(...args)],
-  });
-};
