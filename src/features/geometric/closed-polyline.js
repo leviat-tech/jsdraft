b537ee0a8331b8a996ba96aac51d86ca498610a5
Stephen <squebe@crh.io>
2021-02-19 14:43:05 -0700

    Remove polyline features that map to face and polycurve

--- src/features/geometric/closed-polyline.js
+++
@@ -1,8 +0,0 @@
-const ClosedPolyline = require('../../entities/geometric/closed-polyline.js');
-
-
-module.exports = function closed_polyline(sketch, ...args) {
-  return sketch.create({
-    entities: [new ClosedPolyline(...args)],
-  });
-};
--- src/features/geometric/polyline.js
+++
@@ -1,6 +0,0 @@
-const polyline_bulge = require('./polyline-bulge.js');
-
-
-module.exports = function polyline(...args) {
-  return polyline_bulge(...args);
-};
