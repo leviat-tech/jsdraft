749feca82c490bbc7924315de76302e632c70f17
Stephen <squebe@crh.io>
2021-02-25 15:32:59 -0700

    Use new constructor strategy in arc entity

--- src/entities/geometric/arc.js
+++ src/entities/geometric/arc.js
@@ -1,18 +1,57 @@
-const math = require('mathjs')
 const flatten = require('@flatten-js/core');
+const { normalize, matches } = require('../../utility/arguments');
 
 
 class Arc extends flatten.Arc {
   constructor(...args) {
+    args = normalize(args);
 
-    // construct from four numbers (x, y, r, s, e, cc)
-    if (!(args[0] instanceof flatten.Point)) {
-      return super(flatten.point(args[0], args[1]), ...args.slice(2));
+    if (matches(args, 'point', 'number', 'number', 'number', '...')) {
+      const [center, radius, start, end, cc] = args;
+      return super(flatten.point(...center), radius, start, end, cc);
     }
 
-    // construct from pt, r, start, end, cc
-    return super(...args)
+    if (matches(args, 'point', 'point', 'number', '...')) {
+      const [center, start, angle, cc] = args;
+      return Arc.from_center_start(center, start, angle, cc);
+    }
+
+    if (matches(args, 'point', 'point', 'point')) {
+      const [start, through, end] = args;
+      return Arc.from_through_point(start, through, end);
+    }
+
+    if (matches(args, 'point', 'number', 'point')) {
+      const [start, bulge, end] = args;
+      return Arc.from_bulge(start, bulge, end);
+    }
+
+    if (matches('segment', 'segment', 'number', '...')) {
+      const [tan_a, tab_b, radius, side] = args;
+      return Arc.from_tangents(tan_a, tab_b, radius, side);
+    }
   }
+
+  static from_center_radius(center, radius, start, end, cc) {
+    return new Arc(center, radius, start, end, cc);
+  }
+
+  static from_center_start(center, start, angle, cc) {
+    return new Arc(center, start, angle, cc);
+  }
+
+  static from_through_point(start, through, end) {
+    return new Arc(start, through, end);
+  }
+
+  static from_bulge(start, bulge, end) {
+    return new Arc(start, bulge, end);
+  }
+
+  static from_tangents(tan_a, tab_b, radius, side) {
+    return new Arc(tan_a, tab_b, radius, side);
+  }
+
 }
 
 
