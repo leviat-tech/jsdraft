9786edeac729769aff432a64d022c20ede323f42
Stephen <squebe@crh.io>
2021-02-25 15:33:07 -0700

    Use new constructor strategy for circle entity

--- src/entities/geometric/circle.js
+++ src/entities/geometric/circle.js
@@ -1,27 +1,47 @@
-const math = require('mathjs')
 const flatten = require('@flatten-js/core');
+const Polyface = require('./polyface');
+const { normalize, matches } = require('../../utility/arguments');
 
 
-class Circle extends flatten.Circle {
+class Circle extends Polyface {
   constructor(...args) {
-    // construct from 3 numbers (x, y, r)
-    if (!(args[0] instanceof flatten.Point)) {
-      return super(flatten.point(args[0], args[1]), args[2]);
+    args = normalize(args);
+
+    if (matches(args, 'point', 'number')) {
+      const [center, radius] = args;
+      return super(flatten.circle(flatten.point(...center), radius));
     }
-    // construct from point and radius
-    return super(...args)
+
+    if (matches(args, 'point', 'point')) {
+      const [a, b] = args;
+      return Circle.from_two_points(a, b);
+    }
+
+    if (matches(args, 'point', 'point', 'point')) {
+      const [a, b, c] = args;
+      return Circle.from_two_points(a, b, c);
+    }
+
+    if (matches(args, 'segment', 'segment', 'number')) {
+      const [tan_a, tan_b, radius] = args;
+      return Circle.from_two_points(tan_a, tan_b, radius);
+    }
   }
 
-  transform(matrix = new Flatten.Matrix()) {
-    // breaks x, y scaling would need be paramaterized with 2 vectors and center
-    // arcs are also only defined on a circle and break under some affine transforms
-    // at the very least we need to validate the transformation matrix
-    // and we should add that to arcs as well
-    const p = flatten.point(this.pc.x, this.pc.y + this.r);
-    const t_p = p.transform(matrix);
-    const t_pc = this.pc.transform(matrix);
-    const t_r = t_pc.distanceTo(t_p)[0];
-    return new Circle(t_pc, t_r);
+  static from_center_radius(center, radius) {
+    return new Circle(center, radius);
+  }
+
+  static from_two_points(a, b) {
+    return new Circle(a, b);
+  }
+
+  static from_three_points(a, b, c) {
+    return new Circle(a, b, c);
+  }
+
+  static from_tangents(tan_a, tan_b, radius) {
+    return new Circle(tan_a, tan_b, radius);
   }
 }
 
