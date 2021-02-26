3b9a572a87ca406cb440b10b41900932920d80d0
Stephen <squebe@crh.io>
2021-02-26 16:40:52 -0700

    Revert "Change normalize to return shapes not arrays"
    This reverts commit 678f9e2639ec8eb22d0854bf935aaa419b045f35.

--- src/utility/arguments/normalize.js
+++ src/utility/arguments/normalize.js
@@ -1,19 +1,12 @@
 const flatten = require('@flatten-js/core');
-const { matches } = require('./matches');
 
 
 function normalize(args) {
-  if (!Array.isArray(args)) {
-    return args;
+  if (Array.isArray(args)) {
+    return args.map((a) => normalize(a));
   }
-  if (matches([args], 'point')) {
-    return flatten.point(...args);
-  }
-  if (matches([args], 'segment')) {
-    return flatten.segment(flatten.point(...args[0]), flatten.point(...args[1]));
-  }
-  if (matches([args], 'arc')) {
-    return flatten.arc(flatten.point(...args[0]), ...args.slice(1));
+  if (args instanceof flatten.Point) {
+    return [args.x, args.y];
   }
   return args;
 }
--- test/utility/arguments/normalize.spec.js
+++ test/utility/arguments/normalize.spec.js
@@ -2,10 +2,7 @@
 const { expect } = require('chai');
 const flatten = require('@flatten-js/core');
 const normalize = require('../../../src/utility/arguments/normalize');
-
-
-const pa = flatten.point(1, 2);
-const pb = flatten.point(3, 4);
+const Point = require('../../../src/entities/geometric/point');
 
 
 describe('normalize', () => {
@@ -20,17 +17,18 @@ describe('normalize', () => {
     expect(normalize([[1], [[]]])).to.eql([[1], [[]]]);
   });
 
-  it('will convert numeric 2-arrays to point', () => {
-    expect(normalize([1, 2])).to.eql(flatten.point(1, 2));
+  it('will convert points to arrays', () => {
+    expect(normalize(flatten.point(1, 2))).to.eql([1, 2]);
+    expect(normalize(new Point(1, 2))).to.eql([1, 2]);
   });
 
-  it('will convert array to segment', () => {
-    expect(normalize([[1, 2], [3, 4]])).to.eql(flatten.segment(pa, pb));
+  it('will convert points to arrays', () => {
+    expect(normalize(flatten.point(1, 2))).to.eql([1, 2]);
+    expect(normalize(new Point(1, 2))).to.eql([1, 2]);
   });
 
-  it('will convert array to arc', () => {
-    expect(normalize([[1, 2], 3, 4])).to.eql(flatten.arc(pa, 3, 4));
-    expect(normalize([[1, 2], 3, 4, true])).to.eql(flatten.arc(pa, 3, 4, true));
+  it('will convert nested shapes', () => {
+    expect(normalize([[flatten.point(1, 2)], 3])).to.eql([[[1, 2]], 3]);
   });
 
 });
