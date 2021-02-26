70579573d6ef9985ded3e34b1b02bf5ae857ffbd
Stephen <squebe@crh.io>
2021-02-26 15:39:05 -0700

    Fix segment feature lint errors

--- src/features/geometric/segment.js
+++ src/features/geometric/segment.js
@@ -3,6 +3,6 @@ const Segment = require('../../entities/geometric/segment.js');
 
 module.exports = function segment(sketch, ...args) {
   return sketch.create({
-    entities: [new Segment(...args)]
+    entities: [new Segment(...args)],
   });
-}
+};
