06c478d47301d7578409bc5377d4f37b6397a9da
Stephen <squebe@crh.io>
2021-02-26 17:09:16 -0700

    Normalize segments and arcs

--- test/utility/arguments/normalize.spec.js
+++ test/utility/arguments/normalize.spec.js
@@ -17,18 +17,22 @@ describe('normalize', () => {
     expect(normalize([[1], [[]]])).to.eql([[1], [[]]]);
   });
 
-  it('will convert points to arrays', () => {
+  it('will convert points to array representation', () => {
     expect(normalize(flatten.point(1, 2))).to.eql([1, 2]);
     expect(normalize(new Point(1, 2))).to.eql([1, 2]);
   });
 
-  it('will convert points to arrays', () => {
-    expect(normalize(flatten.point(1, 2))).to.eql([1, 2]);
-    expect(normalize(new Point(1, 2))).to.eql([1, 2]);
+  it('will convert segment to array representation', () => {
+    expect(normalize(flatten.segment(flatten.point(1, 2), flatten.point(3, 4))))
+      .to.eql([[1, 2], [3, 4]]);
   });
 
-  it('will convert nested shapes', () => {
-    expect(normalize([[flatten.point(1, 2)], 3])).to.eql([[[1, 2]], 3]);
+  it('will convert arc to array representation', () => {
+    const arc_a = flatten.arc(flatten.point(1, 2), 3, 4, 5);
+    const ary = normalize(arc_a);
+    const arc_b = flatten.arc(flatten.point(...ary[0]), ary[1], ary[2], ary[3], ary[4]);
+    expect(normalize(arc_a)).to.eql([[1, 2], 3, 4, 5, true]);
+    expect(arc_a).to.eql(arc_b);
   });
 
 });
