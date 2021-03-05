3e773cb659defdbc11a47a39009778abe953a5e2
Stephen <squebe@crh.io>
2021-03-05 15:33:06 -0700

    Add polyface construction functions and tests

--- src/entities/geometric/polyface.js
+++ src/entities/geometric/polyface.js
@@ -1,40 +1,61 @@
 const flatten = require('@flatten-js/core');
-const { normalize, matches } = require('../../utility/arguments');
+const { normalize, every, matches } = require('../../utility/arguments');
+const Polycurve = require('./polycurve.js');
 
 
 class Polyface extends flatten.Polygon {
   constructor(...args) {
     args = normalize(args);
 
-    // if (matches(args, '...', 'point', '<number>')) {
-    //
-    // }
-    //
-    // if (matches(args, '...', 'point and number')) {
-    //   return Polyface.from_bulge();
-    // }
-    //
-    // if (matches(args, '...', 'segment or arc')) {
-    //   return Polyface.from_segments();
-    // }
+    if (matches(args, 'polyface')) {
+      return Polyface.from_polyface(...args);
+    }
+
+    if (matches(args, 'polycurve')) {
+      return Polyface.from_polycurve(...args);
+    }
+
+    if (every(args, ['point', 'number'])) {
+      return Polyface.from_fillet(...args);
+    }
+
+    if (every(args, 'point or number')) {
+      return Polyface.from_bulge(...args);
+    }
+
+    if (every(args, 'segment or arc')) {
+      return Polyface.from_segments(...args);
+    }
 
     return super(...args);
   }
 
-  static from_fillet(center, radius) {
-    return new Polyface(center, radius);
+  static from_polyface(polyface) {
+    const p = new Polyface();
+    for (const face of polyface.faces) {
+      p.addFace(face.shapes);
+    }
+    return p;
   }
 
-  static from_bulge(a, b) {
-    return new Polyface(a, b);
+  static from_polycurve(polycurve) {
+    const p = new Polyface();
+    const vertices = polycurve.vertices;
+    const closing = flatten.segment(vertices[vertices.length - 1], vertices[0]);
+    p.addFace([...polycurve.toShapes(), closing]);
+    return p;
   }
 
-  static from_segments(a, b, c) {
-    return new Polyface(a, b, c);
+  static from_fillet(...points) {
+    return this.from_polycurve(new Polycurve(...points));
   }
 
-  static from_shapes(a, b, c) {
-    return new Polyface(a, b, c);
+  static from_bulge(...args) {
+    return this.from_polycurve(new Polycurve(...args));
+  }
+
+  static from_segments(...segs) {
+    return this.from_polycurve(new Polycurve(...segs));
   }
 }
 
---
+++ test/entities/geometric/polyface.spec.js
@@ -0,0 +1,82 @@
+/* global describe, it */
+/* eslint-disable no-unused-expressions */
+const { expect, Assertion } = require('chai');
+const flatten = require('@flatten-js/core');
+const Rectangle = require('../../../src/entities/geometric/rectangle');
+const Polycurve = require('../../../src/entities/geometric/polycurve');
+const Polyface = require('../../../src/entities/geometric/polyface');
+
+
+Assertion.addMethod('polyface', function (vertices) {
+  new Assertion(this._obj).to.be.instanceof(Polyface);
+  if (typeof (vertices) === 'number') {
+    new Assertion(this._obj.vertices.length).to.eql(vertices);
+  } else {
+    expect(this._obj.vertices).to.eql(vertices.map((v) => flatten.point(...v)));
+  }
+});
+
+
+describe('Polyface', () => {
+
+  it('can be constructed from a polyface', () => {
+    const c = new Polyface(new Rectangle(0, 0, 1, 1));
+    const v = [
+      [0, 0],
+      [1, 0],
+      [1, 1],
+      [0, 1],
+    ];
+    expect(c).to.be.a.polyface(v);
+    expect(new Polyface(c)).to.be.a.polyface(v);
+  });
+
+  it('can be constructed from a polycurve', () => {
+    const v = [
+      [0, 0],
+      [1, 0],
+      [1, 1],
+      [0, 1],
+    ];
+    const pc = new Polycurve(...v);
+    const pf = new Polyface(pc);
+    expect(pf).to.be.a.polyface(v);
+  });
+
+  it('can be constructed from segments', () => {
+    const c = new Polyface(
+      [[0, 0], [1, 1]],
+      [[1, 1], [2, 0]],
+      [[2, 0], [5, 5]],
+    );
+    expect(c).to.be.a.polyface([
+      [0, 0],
+      [1, 1],
+      [2, 0],
+      [5, 5],
+    ]);
+  });
+
+  it('can be constructed from points and bulges', () => {
+    const c = new Polyface(
+      [0, 0],
+      [3, 3],
+      1,
+      [5, 4],
+      [8, 2],
+      1,
+      [4, 0],
+    );
+    expect(c).to.be.a.polyface(5);
+  });
+
+  it('can be constructed from points and fillets', () => {
+    const c = new Polyface(
+      [[0, 0], 0],
+      [[10, 10], 2],
+      [[20, 0], 1.5],
+      [[30, 10], 0],
+    );
+    expect(c).to.be.a.polyface(6);
+  });
+});
