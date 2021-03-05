e9d5d1c339fcaeca291aaa9b3aad24a536901689
Stephen <squebe@crh.io>
2021-03-05 15:31:40 -0700

    Don't reuse the arguments reserved word in render

--- src/render.js
+++ src/render.js
@@ -5,30 +5,30 @@ const load = require('./loaders/load');
 const Sketch = require('./sketch/sketch.js');
 
 
-function main(renderer, feature, arguments, options) {
+function main(renderer, feature, arg, options) {
   // parse parameters
-  const params = arguments.map(a => JSON.parse(a));
+  const params = args.map((a) => JSON.parse(a));
 
   // import desired render function
   const render = require(`./renderers/${renderer}.js`);
 
   // import root feature function
-  const func = load(path.join(process.cwd(), feature))
+  const func = load(path.join(process.cwd(), feature));
 
   // create blank sketch to pass to root feature function
   const blank = new Sketch();
 
   // execute root feature function and render
-  const sketch = func(blank, ...arguments);
+  const sketch = func(blank, ...params);
   render(sketch, options);
 }
 
 program
-  .arguments('<renderer> <feature> [arguments...]')
+  .arguments('<renderer> <feature> [args...]')
   .description('renderer', {
     renderer: 'render function',
     feature: 'root feature function to generate sketch',
-    arguments: 'json arguments to pass root feature function',
+    args: 'json args to pass root feature function',
   })
   .option('-f , --file <path>', 'output file path')
   .action(main);
