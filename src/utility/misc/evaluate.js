af8e6511d3036e825c535502c0e9030cf418081a
Stephen <squebe@crh.io>
2021-03-05 19:27:41 -0700

    Refactor so safe js evaluation can be adjusted in a single place

---
+++ src/utility/misc/evaluate.js
@@ -0,0 +1,19 @@
+const safe = require('notevil');
+
+
+/*
+WARNING: THIS IS INSECURE: USE SECURE PARSING AND EVALUATION TO PREVENT POISONED DRAFT FILES
+============================================================================================
+Curretly "notevil" isn't used because it doesn't have perfect parity with JS.  For example:
+return sketch.draw(
+  sketch.point([0, 0]),
+  sketch.point([25, 25]), // <<<<<<
+)
+Breaks because of the trailing comma on the last argument. At least lets create a test case
+that breaks or warns until secure evaluation is completed.
+*/
+function evaluate(expression) {
+  return eval(expression);
+}
+
+module.exports = evaluate;
