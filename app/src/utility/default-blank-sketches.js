80f2536fcc4270160e04ff6ef657e4c89e0ab754
Stephen <squebe@crh.io>
2021-03-05 21:57:21 -0700

    Change default blank sketches to render a circle

--- app/src/utility/default-blank-sketches.js
+++ app/src/utility/default-blank-sketches.js
@@ -3,10 +3,11 @@ import snakeCase from 'lodash/snakeCase';
 
 const yaml = () => `parameters:
 sketch:
+  - circle: [[0, 0], 10]
 `;
 
 const js = (name) => `function ${snakeCase(name)} (sketch) {
-  return sketch;
+  return sketch.circle([0, 0], 10);
 }
 `;
 
