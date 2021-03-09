9c0b6d19d2106d2f0d405812cb736fd434be8275
David Anderson <danderson@crh.io>
2021-03-08 21:29:20 -0500

    Translate style props to svg attributes

--- src/renderers/svg.js
+++ src/renderers/svg.js
@@ -9,6 +9,18 @@ const DEFAULT_ATTRIBUTES = {
   vector_effect: 'non-scaling-stroke',
 };
 
+function style_to_svg_attributes(style) {
+  return {
+    ...(style.stroke?.width && { stroke_width: style.stroke.width }),
+    ...(style.stroke?.color && { stroke: style.stroke.color }),
+    ...(style.stroke?.scaled && { vector_effect: 'none' }),
+    ...(style.stroke?.pattern && { stroke_dasharray: style.stroke.pattern }),
+    ...(style.stroke?.opacity && { stroke_opacity: style.stroke.opacity }),
+    ...(style.fill?.color && { fill: style.fill.color }),
+    ...(style.fill?.opacity && { fill_opacity: style.fill.opacity }),
+    ...(style.opacity && { opacity: style.opacity }),
+  };
+}
 
 function entity_type(entity) {
   if (entity instanceof flatten.Point) return 'point';
@@ -87,7 +99,7 @@ function recurse(svg, sketch, style) {
     const type = entity_type(entity);
     const styles = {
       ...DEFAULT_ATTRIBUTES,
-      ...s,
+      ...style_to_svg_attributes(s),
     };
 
     svg += `\n${entity_svg[type](entity, styles)}`;
