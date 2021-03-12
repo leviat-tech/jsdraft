4434d3271e8702bade3cea92c8ac10a03333ca6b
David Anderson <danderson@crh.io>
2021-03-11 21:30:19 -0500

    Provide utility functions to main export

--- src/renderers/svg.js
+++ src/renderers/svg.js
@@ -1,5 +1,4 @@
-const flatten = require('@flatten-js/core');
-const svg_string = require('../utility/misc/svg-string.js');
+const entity_to_svg = require('../utility/misc/entity-to-svg.js');
 
 
 const DEFAULT_ATTRIBUTES = {
@@ -22,74 +21,6 @@ function style_to_svg_attributes(style) {
   };
 }
 
-function entity_type(entity) {
-  if (entity instanceof flatten.Point) return 'point';
-  if (entity instanceof flatten.Segment) return 'segment';
-  if (entity instanceof flatten.Arc) return 'arc';
-  if (entity instanceof flatten.Multiline) return 'polycurve';
-  if (entity instanceof flatten.Polygon) return 'polyface';
-  return null;
-}
-
-const entity_svg = {
-  // Point renders as a quasi-zero-size point, drawn with a 10px diameter pen
-  point: (e, styles) => {
-    const attributes = {
-      ...styles,
-      d: `M${e.x},${e.y} L${e.x},${e.y + 0.0001}`,
-      stroke_linecap: 'round',
-      stroke_width: '10px',
-      vector_effect: 'non-scaling-stroke',
-    };
-    return svg_string('path', attributes);
-  },
-
-  segment: (e, styles) => {
-    const d = `M${e.start.x},${e.start.y} L${e.end.x},${e.end.y}`;
-    const attributes = {
-      ...styles,
-      d,
-    };
-    return svg_string('path', attributes);
-  },
-
-  arc: (e, styles) => {
-    const laf = e.sweep <= Math.PI ? '0' : '1';
-    const sf = e.counterClockwise ? '1' : '0';
-    const d = `M${e.start.x},${e.start.y} A${e.r},${e.r},${laf},${sf},${e.end.x},${e.end.y}`;
-    const attributes = {
-      ...styles,
-      d,
-    };
-    return svg_string('path', attributes);
-  },
-
-  // Polycurves should not have a closing "Z" segment drawn
-  polycurve: (e, styles) => {
-    let d = `M${e.first.start.x},${e.first.start.y}`;
-    for (const edge of e) { d += edge.svg(); }
-
-    const attributes = {
-      ...styles,
-      d,
-    };
-
-    return svg_string('path', attributes);
-  },
-
-  polyface: (e, styles) => {
-    let d = '';
-    for (const face of e.faces) { d += face.svg(); }
-
-    const attributes = {
-      ...styles,
-      d,
-    };
-
-    return svg_string('path', attributes);
-  },
-};
-
 function recurse(sketch, style) {
   let svg = '';
 
@@ -98,13 +29,12 @@ function recurse(sketch, style) {
 
   // draw entities
   for (const entity of sketch.node.entities) {
-    const type = entity_type(entity);
     const styles = {
       ...DEFAULT_ATTRIBUTES,
       ...style_to_svg_attributes(s),
     };
 
-    svg += `\n${entity_svg[type](entity, styles)}`;
+    svg += `\n${entity_to_svg(entity, styles)}`;
   }
 
   // draw children
