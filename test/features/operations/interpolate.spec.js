cb6736b6951f5876a4dbeda19c3648943dd9889c
David Anderson <danderson@crh.io>
2021-03-14 09:39:08 -0400

    Add interpolate operation

---
+++ test/features/operations/interpolate.spec.js
@@ -0,0 +1,28 @@
+/* global describe, it */
+
+const chai = require('chai');
+const Sketch = require('../../../src/sketch/sketch.js');
+
+
+chai.expect();
+
+const { expect } = chai;
+
+describe('Interpolate', () => {
+  const block = new Sketch()
+    .polycurve(
+      [-5, 0],
+      [-5, 5],
+      1,
+      [5, 5],
+      [5, 0],
+    );
+
+  const sketch = new Sketch()
+    .interpolate(block, [0, 0], [50, 50], [10, 25, 40]);
+
+  it.only('should place blocks along a line', () => {
+    const pcurve = sketch.shape;
+    expect(pcurve.vertices.length).to.eql(14);
+  });
+});
