67b50fd4e92f4aa40912d2f5bc9fb32c6018c1f0
Stephen <squebe@crh.io>
2021-03-17 16:35:58 -0600

    Change stroke to have color first so it aligns with fill

--- src/features/style/stroke.js
+++ src/features/style/stroke.js
@@ -1,4 +1,4 @@
-module.exports = function stroke(sketch, width, color) {
-  sketch.node.style.stroke = {width, color};
+module.exports = function stroke(sketch, color, width) {
+  sketch.node.style.stroke = { width, color };
   return sketch;
-}
+};
