7c95323659a6c21eb8c605a429877cee8260eb3c
Stephen <squebe@crh.io>
2021-03-17 16:17:15 -0600

    Fix basic js draft example file

--- examples/basic.draft.js
+++ examples/basic.draft.js
@@ -1,9 +1,8 @@
 /* eslint-disable */
 function basic(sketch) {
-  return sketch.draw(
-    sketch.point([0, 0]),
-    sketch.point([25, 25])
-  )
+  return sketch
+    .point([0, 0])
+    .point([25, 25])
     .fill('blue')
     .stroke(2, 'red');
 }
