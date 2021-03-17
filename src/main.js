394951ab20b29e86e05e775693af14f13786aba0
David Anderson <danderson@crh.io>
2021-03-17 17:32:42 -0400

    Remove utility functions from main export

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
 import Draft from './draft.js';
 import Sketch from './sketch/sketch.js';
-import { entity_type } from './utility/misc/entity-type.js';
-import entity_to_svg from './utility/misc/entity-to-svg.js';
 
 
 export {
   Draft,
   Sketch,
-  entity_type,
-  entity_to_svg,
 };
 
 export default Sketch;
