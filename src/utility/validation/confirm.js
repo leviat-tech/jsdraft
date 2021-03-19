2db23b7ee8ca43da458c1a1c100a4d28ce4b6270
Stephen <squebe@crh.io>
2021-03-19 16:36:34 -0600

    Add confirm function to confirm a value is of type or is entity instance

---
+++ src/utility/validation/confirm.js
@@ -0,0 +1,15 @@
+const isPlainObject = require('lodash/isPlainObject');
+const entities = require('../../entities');
+const capitalize = require('./capitalize');
+const types = require('./types');
+
+
+function confirm(value, type) {
+  if (type === 'object') return isPlainObject(value);
+  if (type === 'array') return Array.isArray(value);
+  if (types.standard.includes(type)) return typeof value === type; // eslint-disable-line valid-typeof
+  if (types.entity.includes(type)) return value instanceof entities[capitalize(type)];
+  throw new TypeError(`Unrecognized validation type: ${type}`);
+}
+
+module.exports = confirm;
