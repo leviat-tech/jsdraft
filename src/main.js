351677748f9f6c5f48b530efb01e369535b91ded
David Anderson <danderson@crh.io>
2021-03-26 18:54:12 -0400

    Export render function

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Draft from './draft.js';
 import Sketch from './sketch/sketch.js';
+import render from './render.js';
 
 
 export {
   Draft,
   Sketch,
+  render,
 };
 
 export default Sketch;
