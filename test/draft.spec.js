33547726d03b901c11c92893354a7a0414663887
David Anderson <danderson@crh.io>
2021-03-27 11:22:17 -0400

    Remove extraneous test file

--- test/draft.spec.js
+++ test/draft.spec.js
@@ -32,4 +32,40 @@ describe('draft', () => {
     draft.add_file('composed', 'js', composed);
     expect(draft).to.render({ name: 'point', x: 0, y: 0 }, { sketch: 'composed' });
   });
+
+  it('should be able to parse and render a javascript sketch', () => {
+    const draft = new Draft();
+    draft.add_file('my_point', 'js', `
+      function feature(sketch, a, b) {
+        return sketch.point(a, b);
+      }
+    `);
+    const result = draft.render('my_point', [3, 5], 'svg');
+    expect(result).to.contain('path');
+  });
+
+  it('should be able to parse and render a yaml sketch', () => {
+    const draft = new Draft();
+    draft.add_file('my_point', 'yaml', `
+      parameters:
+        - $x: 5
+      reference:
+        - $y: '10'
+      sketch:
+        - point: [$x, $y]
+    `);
+    const result = draft.render('my_point', [3, 5], 'svg');
+    expect(result).to.contain('path');
+  });
+
+  it('should be able to parse and render the default javascript sketch', () => {
+    const draft = new Draft();
+    draft.add_file('feature', 'js', `
+    function untitled (sketch, args) {
+      return sketch;
+    }
+    `);
+    const result = draft.render('feature', [], 'svg');
+    expect(result).to.contain('svg');
+  });
 });
