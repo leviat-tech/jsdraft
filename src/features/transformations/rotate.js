7e44fd12fbf5b6f84c11c6512d0f57c3155d7974
David Anderson <danderson@crh.io>
2021-03-29 11:14:19 -0400

    Remove nested conditional

--- src/features/transformations/rotate.js
+++ src/features/transformations/rotate.js
@@ -4,14 +4,12 @@ const { deg_to_rad } = require('../../utility/misc/rad-deg');
 
 
 module.exports = function rotate(sketch, angle, units = 'deg') {
-  if (typeof angle === 'number') {
-    if (units === 'deg') {
-      angle = deg_to_rad(angle);
-    }
-  } else {
+  if (typeof angle !== 'number') {
     // angle is vector
     const vec = Vector(angle);
     angle = vec.angle();
+  } else if (units === 'deg') {
+    angle = deg_to_rad(angle);
   }
 
   return sketch.transform(flatten.matrix().rotate(angle));
