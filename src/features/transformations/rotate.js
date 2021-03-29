5af21a81c817fb5feaf13f4087fb2defa0b2ffe0
David Anderson <danderson@crh.io>
2021-03-28 20:46:53 -0400

    Support vector angle for rotation

--- src/features/transformations/rotate.js
+++ src/features/transformations/rotate.js
@@ -1,8 +1,24 @@
 const flatten = require('@flatten-js/core');
+const Vector = require('@crhio/vector').default;
+const { deg_to_rad } = require('../../utility/misc/rad-deg');
 
-module.exports = function rotate(sketch, angle, units) {
-  if (!units) {
-    angle = angle * Math.PI / 180.0
+
+module.exports = function rotate(sketch, angle, units = 'deg') {
+  if (typeof angle === 'number') {
+    if (units === 'deg') {
+      angle = deg_to_rad(angle);
+    }
+  } else {
+    // angle is vector
+    let vec;
+    if (Array.isArray(angle)) {
+      vec = Vector({ x: angle[0], y: angle[1] });
+    } else {
+      vec = Vector({ x: angle.x, y: angle.y });
+    }
+
+    angle = vec.angle;
   }
+
   return sketch.transform(flatten.matrix().rotate(angle));
-}
+};
