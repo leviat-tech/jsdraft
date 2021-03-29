7e548228d7fcf79bdbb61f8c16ddcf1d6826e266
Stephen <squebe@crh.io>
2021-03-29 15:21:31 -0600

    Export parse function from draft library

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Draft from './draft.js';
 import Sketch from './sketch/sketch.js';
 import render from './render.js';
+import parse from './loaders/parse.js';
 
 
 export {
   Draft,
   Sketch,
   render,
+  parse,
 };
 
 export default Sketch;
