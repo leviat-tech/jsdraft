ec07d45fcf683f31150097eb08da180fe373a268
David Anderson <danderson@crh.io>
2021-03-30 19:59:38 -0400

    Show hovered entities in drawing

--- app/src/components/drawing/Drawing.vue
+++ app/src/components/drawing/Drawing.vue
@@ -1,15 +1,29 @@
 <template>
-  <g v-html="svg" />
+  <g>
+    <g v-html="svg" />
+    <selected-entities
+      v-if="hoveredEntities.length > 0"
+      :entities="hoveredEntities"
+    />
+  </g>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapState, mapGetters } from 'vuex';
+import SelectedEntities from './SelectedEntities.vue';
 
 
 export default {
   name: 'Drawing',
+  components: {
+    SelectedEntities,
+  },
   computed: {
-    ...mapGetters(['svg']),
+    ...mapState(['hovered']),
+    ...mapGetters(['svg', 'entities']),
+    hoveredEntities() {
+      return Object.keys(this.hovered).map((index) => this.entities[index]);
+    },
   },
 };
 </script>
---
+++ app/src/components/drawing/SelectedEntities.vue
@@ -0,0 +1,37 @@
+<template>
+  <g>
+    <g
+      v-for="entity, i in svg"
+      :key="i"
+      v-html="entity"
+    />
+  </g>
+</template>
+
+<script>
+import { render } from '../../../../dist/draft.js';
+
+
+export default {
+  name: 'SelectedEntities',
+  props: {
+    entities: { type: Array, default: () => [] },
+    stroke: { type: String, default: '#3D95F0' },
+    strokeWidth: { type: String, default: '2px' },
+    fill: { type: String, default: 'none' },
+  },
+  computed: {
+    svg() {
+      return this.entities.map((entity) => render(entity, 'svg', {
+        stroke: {
+          color: this.stroke,
+          width: this.strokeWidth,
+        },
+        fill: {
+          color: this.fill,
+        },
+      }));
+    },
+  },
+};
+</script>
