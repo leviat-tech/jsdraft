36487fee9749f9de9d5731a712246ac414b0ff9d
Stephen <squebe@crh.io>
2021-03-29 20:00:13 -0600

    Create error panel component for displaying errors

---
+++ app/src/components/ErrorPanel.vue
@@ -0,0 +1,46 @@
+<template>
+  <div v-show="message" class="error-panel">
+    <div class="header"><h2>Errors</h2></div>
+    <pre class="error">{{ message }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorPanel',
+  props: {
+    errors: {
+      default: () => {},
+      type: Object,
+    },
+  },
+  computed: {
+    message() {
+      if (!this.errors) return '';
+      return this.errors.message;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  @import '../assets/styles/variables.scss';
+
+  .error-panel {
+    .header {
+      padding: .5rem 1rem;
+      border-top: $border-sm solid $color-gray-03;
+      border-bottom: $border-sm solid $color-gray-03;
+      h2 {
+        margin: 0rem;
+      }
+    }
+    .error {
+      padding: 1rem;
+      font-size: 14px;
+      font-family: $font-monospace;
+      color: $color-red;
+      overflow: auto;
+    }
+  }
+</style>
