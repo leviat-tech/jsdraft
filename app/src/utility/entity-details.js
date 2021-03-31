ec40220f182c0b1bec8e8f1d029d924018cb0ef0
David Anderson <danderson@crh.io>
2021-03-31 19:49:59 -0400

    Format entity details

--- app/src/utility/entity-details.js
+++ app/src/utility/entity-details.js
@@ -1,13 +1,45 @@
+// utility function to remove quotes from JSON representation
+function u(o) {
+  const json = JSON.stringify(o, null, 1);
+  return json.replace(/"([^"]+)":/g, '$1:').replace(/\n/g, '');
+}
+
+function format(o) {
+  if (Array.isArray(o)) {
+    return o.map((i) => format(i));
+  }
+
+  if (typeof o === 'object' && o !== null) {
+    const obj = Object.entries(o).reduce((p, [k, v]) => {
+      p[k] = format(v);
+      return p;
+    }, {});
+
+    return u(obj);
+  }
+
+  if (!Number.isNaN(o)) return parseFloat(o.toFixed(5), 10);
+
+  return o;
+}
+
+function f(o) {
+  return Object.entries(o).reduce((obj, [k, v]) => {
+    obj[k] = format(v);
+    return obj;
+  }, {});
+}
+
 const entities = {
-  Point: (e) => ({
+  Point: (e) => f({
     x: e.x,
     y: e.y,
   }),
-  Segment: (e) => ({
+  Segment: (e) => f({
     ps: { x: e.ps.x, y: e.ps.y },
     pe: { x: e.pe.x, y: e.pe.y },
   }),
-  Arc: (e) => ({
+  Arc: (e) => f({
     pc: { x: e.pc.x, y: e.pc.y },
     r: e.r,
     start: { x: e.start.x, y: e.start.y },
@@ -16,29 +48,29 @@ const entities = {
     endAngle: e.endAngle,
     counterClockwise: e.counterClockwise,
   }),
-  Circle: (e) => ({
+  Circle: (e) => f({
     pc: { x: e.center.x, y: e.center.y },
     r: e.radius,
   }),
-  Rectangle: (e) => ({
+  Rectangle: (e) => f({
     xmin: e.xmin,
     xmax: e.xmax,
     ymin: e.ymin,
     ymax: e.ymax,
     r: e.r,
   }),
-  Polycurve: (e) => ({
+  Polycurve: (e) => f({
     vertices: e.vertices.map((v) => ({ x: v.x, y: v.y })),
   }),
-  Polyface: (e) => ({
+  Polyface: (e) => f({
     vertices: e.vertices.map((v) => ({ x: v.x, y: v.y })),
   }),
-  AlignedDim: (e) => ({
+  AlignedDim: (e) => f({
     ps: { x: e.ps.x, y: e.ps.y },
     pe: { x: e.pe.x, y: e.pe.y },
     side: e.side,
   }),
-  Text: (e) => ({
+  Text: (e) => f({
     p: { x: e.p.x, y: e.p.y },
     text: e.text,
     rotation: e.rotation,
