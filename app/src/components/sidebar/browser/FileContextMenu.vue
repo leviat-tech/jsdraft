5955ca716fb181320c2f1608710f15cea969c322
Stephen <squebe@crh.io>
2021-04-05 16:18:32 -0600

    Factor out file context menu and double click to open code

---
+++ app/src/components/sidebar/browser/FileContextMenu.vue
@@ -0,0 +1,39 @@
+<template>
+  <context-menu ref="menu" :items="['Open', 'Rename', 'Delete']" @select="select" />
+</template>
+
+
+<script>
+import ContextMenu from '../../ui/ContextMenu.vue';
+
+
+export default {
+  name: 'FileContextMenu',
+  components: {
+    ContextMenu,
+  },
+  methods: {
+    open(event) {
+      console.log('open');
+      this.$refs.menu.open(event);
+    },
+    select(item) {
+      if (item === 'Open') {
+        this.$store.commit('setCurrentFile', this.file);
+      } else if (item === 'Rename') {
+        this.$refs.rename.open();
+      } else if (item === 'Delete') {
+        this.$refs.delete.open();
+      }
+    },
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+  @import '../../../assets/styles/variables.scss';
+
+  .file-context-menu {
+  }
+</style>
