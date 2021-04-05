1a1051c1565a2457fcbd0b1fb92db0bfeb311055
Stephen <squebe@crh.io>
2021-04-05 12:31:26 -0600

    Add modal for removing file via file manager widget

---
+++ app/src/components/sidebar/browser/DeleteModal.vue
@@ -0,0 +1,62 @@
+<template>
+  <warning-modal
+    v-if="showing"
+    :header="false"
+    text=""
+    cancel="Cancel"
+    proceed="Delete"
+    class="delete-modal"
+    @cancel="showing = false"
+    @proceed="remove"
+  >
+    <div class="instructions">
+      Are you sure you wish to delete <span class="highlight">{{ file }}</span> ?
+    </div>
+  </warning-modal>
+</template>
+
+
+<script>
+import WarningModal from '../../WarningModal.vue';
+
+
+export default {
+  name: 'DeleteModal',
+  components: {
+    WarningModal,
+  },
+  props: ['file'],
+  data() {
+    return {
+      showing: false,
+    };
+  },
+  methods: {
+    open() {
+      this.showing = true;
+    },
+    close() {
+      this.showing = false;
+    },
+    remove() {
+      this.$store.commit('removeFile', this.file);
+    },
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+  @import '../../../assets/styles/variables.scss';
+
+  .delete-modal {
+    .instructions {
+      color: $color-gray-09;
+      font-size: .9rem;
+      padding: .5rem 0rem 1.5rem 0rem;
+    }
+    .highlight {
+      font-weight: bold;
+    }
+  }
+</style>
