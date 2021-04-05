08358089c7ecaf66f794c66fc1eff17e22a8fa53
Stephen <squebe@crh.io>
2021-04-05 12:34:28 -0600

    Create new file manager component

---
+++ app/src/components/sidebar/browser/FileBrowser.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="file-browser">
+    <file-header />
+    <file-tab v-for="file in files" :key="file" :file="file" />
+  </div>
+</template>
+
+
+<script>
+import FileHeader from './FileHeader.vue';
+import FileTab from './FileTab.vue';
+
+
+export default {
+  name: 'FileBrowser',
+  components: {
+    FileHeader,
+    FileTab,
+  },
+  computed: {
+    files() { return Object.keys(this.$store.state.files).sort(); },
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+  @import '../../../assets/styles/variables.scss';
+  .file-browser {}
+</style>
