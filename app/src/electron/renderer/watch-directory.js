bcf03e89d043dce55136b7e57853ccffc6fdcac3
Stephen <squebe@crh.io>
2021-04-06 16:45:57 -0600

    Simplify and fix electron directory watcher

--- app/src/electron/renderer/watch-directory.js
+++ app/src/electron/renderer/watch-directory.js
@@ -1,44 +1,21 @@
 const chokidar = require('chokidar');
 const fs = require('fs').promises;
-const path = require('path');
-const parseFilename = require('../utility/parse-filename.js');
+const { basename } = require('path');
+// const parseFilename = require('../utility/parse-filename.js');
 
 
-function watchDirectory(watchPath, updateFile) {
-  const watcher = chokidar.watch(watchPath, {
-    ignored: /[/\\]\./,
-    persistent: true,
+function watchDirectory(directory, commit) {
+  const watcher = chokidar.watch(`${directory}/*.(sketch.js|sketch.yaml)`, { persistent: true });
+
+  watcher.on('add', async (path) => {
+    commit('updateFile', { name: basename(path), code: await fs.readFile(path, 'utf-8') });
   });
-
-  // new/changed files
-  watcher.on('change', async (p) => {
-    const basename = path.basename(p);
-    const dirname = path.dirname(p);
-    const file = parseFilename(basename);
-
-    if (file && dirname === watchPath) {
-      const contents = await fs.readFile(p, 'utf-8');
-      updateFile('change', {
-        ...file,
-        contents,
-      });
-    }
+  watcher.on('change', async (path) => {
+    commit('updateFile', { name: basename(path), code: await fs.readFile(path, 'utf-8') });
   });
-
-  // removed files
-  watcher.on('unlink', async (p) => {
-    const basename = path.basename(p);
-    const dirname = path.dirname(p);
-    const file = parseFilename(basename);
-
-    if (file && dirname === watchPath) {
-      updateFile('unlink', file);
-    }
+  watcher.on('unlink', async (path) => {
+    commit('removeFile', basename(path));
   });
-
-  return async () => {
-    await watcher.close();
-  };
 }
 
 module.exports = watchDirectory;
