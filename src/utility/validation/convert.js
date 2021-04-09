93c2477b51b2d9c15a99d266350adceabf7936bd
Stephen <squebe@crh.io>
2021-04-09 15:27:38 -0600

    Normalize entity types before conversion

--- src/utility/validation/convert.js
+++ src/utility/validation/convert.js
@@ -1,6 +1,7 @@
 const entities = require('../../entities');
 const confirm = require('./confirm');
 const types = require('./types');
+const normalize = require('../arguments/normalize');
 
 
 function convert(value, type) {
@@ -10,7 +11,13 @@ function convert(value, type) {
   if (type === 'string') return String(value);
   if (type === 'array') return JSON.parse(String(value));
   if (type === 'object') return JSON.parse(String(value));
-  if (types.entity.includes(type)) return entities[type](...value);
+  if (types.entity.includes(type)) {
+    value = normalize(value);
+    if (Symbol.iterator in Object(value)) {
+      return entities[type](...value);
+    }
+    return entities[type](value);
+  }
   throw new TypeError(`Unrecognized validation type: ${type}`);
 }
 
