cec93baf14a85aeec299b403909924e40577bb5c
Stephen <squebe@crh.io>
2021-04-12 15:24:17 -0600

    Fix negative indexes in pick utility function

--- src/utility/misc/pick.js
+++ src/utility/misc/pick.js
@@ -2,7 +2,7 @@ function pick(array, index) {
   if (index === undefined) index = 0;
   if (index === 'first') index = 0;
   if (index === 'last') index = array.length - 1;
-  if (index < 0) index = array.length - index;
+  if (index < 0) index = array.length + index;
   return array[index];
 }
 
