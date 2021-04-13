9906ec30064a57fd59bc318d90f51daa5fc57fe2
Stephen <squebe@crh.io>
2021-04-13 14:12:57 -0600

    Fix openFile method from breaking when dir contained dir with a dot

--- app/src/electron/renderer/open-file.js
+++ app/src/electron/renderer/open-file.js
@@ -1,19 +1,25 @@
 const { ipcRenderer } = require('electron');
-const fs = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
 const parseFilename = require('../utility/parse-filename.js');
 
 
+function isFile(p) {
+  const stat = fs.lstatSync(p);
+  return stat.isFile();
+}
+
 async function openFile() {
   const directory = await ipcRenderer.invoke('open-file');
   const d = directory.filePaths[0];
-  const directoryFiles = await fs.readdir(d);
+  const directoryFiles = await fs.promises.readdir(d);
   const files = directoryFiles
+    .filter((file) => isFile(path.join(d, file)))
     .map((filename) => parseFilename(filename))
     .filter((file) => file)
     .map(async (file) => ({
       ...file,
-      contents: await fs.readFile(path.join(d, file.filename), 'utf-8'),
+      contents: await fs.promises.readFile(path.join(d, file.filename), 'utf-8'),
     }));
 
   return {
