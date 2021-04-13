85d2fff25646e1e9b63e9f403c06808bafb69657
David Anderson <danderson@crh.io>
2021-04-13 15:12:16 -0400

    Update interpolate operation to use sketch

--- src/features/operations/interpolate.js
+++ src/features/operations/interpolate.js
@@ -1,22 +1,28 @@
 const flatten = require('@flatten-js/core');
 const Polycurve = require('../../entities/geometric/polycurve.js');
 const Segment = require('../../entities/geometric/segment.js');
+const { base_entity_type } = require('../../utility/misc/entity-type.js');
 
 
 module.exports = {
   name: 'interpolate',
   parameters: [
-    { name: 'pcurve', cast: 'polycurve' },
     { name: 'pt_a', cast: 'point' },
     { name: 'pt_b', cast: 'point' },
     { name: 'positions', type: 'array' },
   ],
-  func: function interpolate(sketch, pcurve, pt_a, pt_b, positions) {
+  func: function interpolate(sketch, pt_a, pt_b, positions) {
     const vec_a = new flatten.Vector([pt_a.x, pt_a.y]);
     const vec_b = new flatten.Vector([pt_b.x, pt_b.y]);
     const dir = vec_b.subtract(vec_a).normalize();
 
     const angle = Math.atan2(dir.y, dir.x);
+
+    const pcurve = sketch.shape;
+    const type = base_entity_type(pcurve);
+    if (type !== 'polycurve') {
+      throw new TypeError(`Interpolated shape must be a polycurve, ${type} provided`);
+    }
 
     const oriented = pcurve
       .transform(flatten.matrix().rotate(angle).translate(pt_a.x, pt_a.y));
