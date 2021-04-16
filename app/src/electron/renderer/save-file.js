6d69fbc62325c1993ba60d22365a7e501f5ef418
David Anderson <danderson@crh.io>
2021-04-16 16:43:12 -0400

    Add blank `.index.js` to allow for webpack imports
    Webpack loaders must resolve to a file--not a directory. If a directory is
    imported (`import draft from 'file.draft';`), webpack will look for an
    index.js in the directory. By adding this file to the directory on save,
    we make it possible to use a webpack loader that will then find all
    files within the draft file and include them in the compiled bundle.

--- app/src/electron/renderer/save-file.js
+++ app/src/electron/renderer/save-file.js
@@ -8,7 +8,10 @@ async function saveFile(savePath, files) {
       const filename = `${file.name}.sketch.${file.extension}`;
       const filePath = path.join(savePath, filename);
       return fs.writeFile(filePath, file.contents);
-    });
+    })
+
+    // Adding a blank index file allows for simplified importing
+    .concat(fs.writeFile(path.join(savePath, 'index.js'), ''));
 
   await Promise.all(saveFiles);
   return true;
