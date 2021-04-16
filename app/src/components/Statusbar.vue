d425cb6a5666396c36cab80b8faf156af7583ac4
David Anderson <danderson@crh.io>
2021-04-16 10:30:44 -0400

    Move current point display to status bar

--- app/src/components/Statusbar.vue
+++ app/src/components/Statusbar.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="statusbar" />
+  <div class="statusbar">
+    <div class="current-point">
+      x: <span class="coordinate">{{ pointDisplay.x }}</span>
+      y: <span class="coordinate">{{ pointDisplay.y }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
+
 export default {
   name: 'Statusbar',
+  computed: {
+    ...mapState(['currentPoint']),
+    pointDisplay() {
+      return {
+        x: Math.round(this.currentPoint.x),
+        y: Math.round(this.currentPoint.y),
+      };
+    },
+  },
 };
 </script>
 
@@ -15,5 +32,17 @@ export default {
     height: 2rem;
     flex-shrink: 0;
     border-top: $border-sm solid $color-gray-03;
+    display: flex;
+    align-items: center;
+  }
+
+  .current-point {
+    margin-left: 1rem;
+    font-size: $text-sm;
+  }
+
+  .coordinate {
+    display: inline-block;
+    width: 2rem;
   }
 </style>
