80e3b1ca9e57eba4a072937f9bf09ad14105a89b
karimi <mortza@gmail.com>
2021-04-16 16:00:44 -0400

    Add extrusion user entity

--- drafts/wp.draft/wp.js
+++ drafts/wp.draft/wp.js
@@ -4,43 +4,47 @@ return {
     { name: "i1", default: 100 },
     { name: "k2", default: 50 },
     { name: "i2", default: 60 },
-    { name: "side", default: true },
+    { name: "side_lip", default: {h: 10, bw:50, tw:30} },
+    { name: "bottom_lip", default: {h: 10, bw:70, tw:50} },
+    { name: "leg", default: {h: 30, gap: 10, r: 5} },
+    { name: "toe", default: {h: 30, gap: 20, r: 5} },
+    { name: "neck", default: {h: 30, gap: 20, r: 10} },
   ],
-  func: function wp(sketch, k1, i1, k2, i2, side) {
-  const main = sketch.user.main(k1,i1,k2,i2,0);
+  func: function wp(sketch, k1, i1, k2, i2, side_lip, bottom_lip, leg, toe, neck) {
+  const Main = sketch.user.main(k1,i1,k2,i2,0);
   
-  const hole = sketch.circle(main.offset(-10).vertices[3], 5);
+  const Hole = sketch.circle(Main.offset(-10).vertices[3], 5);
     
-  const sideLip = sketch.user.lip(10, 60, 30);
-  const sideLipSnapped = sideLip.snap(main.edge(0), sideLip.edge(0));
+  const SideLip = sketch.user.lip(side_lip.h, side_lip.bw, side_lip.tw);
+  const SideLipSnapped = SideLip.snap(Main.edge(0), SideLip.edge(0));
 
-  const bottomLip = sketch.user.lip(10, 60, 40);
-  const bottomLipSnapped = bottomLip.snap(main.edge(1).reverse(), bottomLip.edge(0), 0, true);
+  const BottomLip = sketch.user.lip(bottom_lip.h, bottom_lip.bw, bottom_lip.tw);
+  const BottomLipSnapped = BottomLip.snap(Main.edge(1).reverse(), BottomLip.edge(0), 0, true);
 
-  const halfPill = sketch.user.halfpill(15, 30);
-  const halfPillSnapped = halfPill.snap(main.edge(0).reverse(), halfPill.edge(4), 15, false);
+  const HalfPill = sketch.user.halfpill(15, 30);
+  const HalfPillSnapped = HalfPill.snap(Main.edge(0).reverse(), HalfPill.edge(4), 15, false);
 
-  const pillVoid = sketch.user.pillvoid(30, 50, 20)
-    .orient([0, 0], [1, 0], main.edge(3).vertices[0], main.edge(3).vertices[1]);
+  const PillVoid = sketch.user.pillvoid(30, 50, 20)
+    .orient([0, 0], [1, 0], Main.edge(3).vertices[0], Main.edge(3).vertices[1]);
 
-  const leg = sketch.rectangle(bottomLip.edge(2).length,30,0,0);
-  const legSnapped = leg.snap(bottomLipSnapped.edge(2), leg.edge(0), 0)
+  const Leg= sketch.user.extrusion(BottomLip.edge(2).length - leg.gap,leg.h,leg.r);
+  const LegSnapped = Leg.snap(BottomLipSnapped.edge(2).reverse(), Leg.edge(0), 0, true);
 
-  const toe = sketch.rectangle(20,10,0,0);
-  const toeSnapped = toe.snap(legSnapped.edge(2), toe.edge(0), 0, true)
+  const Toe = sketch.user.extrusion(Leg.edge(-2).length- toe.gap, toe.h,toe.r);
+  const ToeSnapped = Toe.snap(LegSnapped.edge(-2).reverse(), Toe.edge(0), 0, true)
 
-  const neck = sketch.rectangle(main.edge(3).length, 30,0,0);
-  const neckSnapped = neck.snap(main.edge(3), leg.edge(0), 0)
+  const Neck = sketch.user.extrusion(Main.edge(-1).length - neck.gap, neck.h,neck.r);
+  const NeckSnapped = Neck.snap(Main.edge(-1), Neck.edge(0), 0)
 
  
-  let webPlate = main
-  if (side)
-   webPlate = main.union(sideLipSnapped)
+  let WebPlate = Main
+  if (side_lip)
+   WebPlate = Main.union(SideLipSnapped)
 
-//   .union(bottomLipSnapped).subtract(halfPillSnapped).subtract(pillVoid)
+//   .union(BottomLipSnapped).subtract(HalfPillSnapped).subtract(PillVoid)
 
-  return sketch.add(main, sideLipSnapped, bottomLipSnapped,legSnapped, toeSnapped, halfPillSnapped, pillVoid, hole, main.edge(3), neckSnapped);
-//   return sketch.add(webPlate)
+  return sketch.add(Main, SideLipSnapped, BottomLipSnapped, LegSnapped, ToeSnapped, HalfPillSnapped, PillVoid, Hole, NeckSnapped);
+//   return sketch.add(WebPlate)
 }
 }
 
