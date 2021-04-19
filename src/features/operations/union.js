7ca042bf3387df02f2012035a699cf2923c69e4c
Stephen <squebe@crh.io>
2021-04-19 12:27:30 -0600

    Maintain the root node's style as the unioned node's new style

--- src/features/operations/union.js
+++ src/features/operations/union.js
@@ -5,5 +5,5 @@ const flatten = require('@flatten-js/core');
 module.exports = function union(sketch, ...args) {
   const polyfaces = sketch.polyfaces.concat(...args.map((a) => a.polyfaces || [a]));
   const unioned = polyfaces.reduce((a, b) => flatten.BooleanOperations.unify(a, b));
-  return sketch.create({ entity: unioned });
+  return sketch.create({ entity: unioned, style: sketch.node.style });
 };
