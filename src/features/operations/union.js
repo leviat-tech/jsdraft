0930d000c49c056e677b3e568cb5adde8afbd7b0
Stephen <squebe@crh.io>
2021-04-19 14:21:11 -0600

    Don't merge all but merge all right operands with all left entities

--- src/features/operations/union.js
+++ src/features/operations/union.js
@@ -1,9 +1,13 @@
 const flatten = require('@flatten-js/core');
 
 
-// union all polyface entities in sketch and ..args (if arg not a sketch assume its a polyface entity)
 module.exports = function union(sketch, ...args) {
-  const polyfaces = sketch.polyfaces.concat(...args.map((a) => a.polyfaces || [a]));
-  const unioned = polyfaces.reduce((a, b) => flatten.BooleanOperations.unify(a, b));
-  return sketch.create({ entity: unioned, style: sketch.node.style });
+  const polyfaces = [].concat(...args.map((a) => a.polyfaces || [a]));
+  for (const s of sketch.tree('level')) {
+    if (s.node.entity instanceof flatten.Polygon) {
+      const faces = [s.node.entity].concat(polyfaces);
+      s.node.entity = faces.reduce((a, b) => flatten.BooleanOperations.unify(a, b));
+    }
+  }
+  return sketch;
 };
