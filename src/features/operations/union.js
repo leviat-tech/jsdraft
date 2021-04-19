7fb13504b41751754e553e6224f18efd129dfa5d
Stephen <squebe@crh.io>
2021-04-19 12:48:42 -0600

    Show what happens with naive rewound polygon after union

--- src/features/operations/union.js
+++ src/features/operations/union.js
@@ -1,9 +1,10 @@
 const flatten = require('@flatten-js/core');
-
+const Polyface = require('../../entities/geometric/polyface');
 
 // union all polyface entities in sketch and ..args (if arg not a sketch assume its a polyface entity)
 module.exports = function union(sketch, ...args) {
   const polyfaces = sketch.polyfaces.concat(...args.map((a) => a.polyfaces || [a]));
   const unioned = polyfaces.reduce((a, b) => flatten.BooleanOperations.unify(a, b));
-  return sketch.create({ entity: unioned, style: sketch.node.style });
+  const rewound = new Polyface(flatten.polygon([...unioned.edges].map((e) => e.shape)));
+  return sketch.create({ entity: rewound, style: sketch.node.style });
 };
