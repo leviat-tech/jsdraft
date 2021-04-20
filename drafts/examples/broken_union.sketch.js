516ebbb5e84dd16b2334631c9b463822a91fd7ce
Stephen <squebe@crh.io>
2021-04-20 14:13:38 -0600

    Add example of unified shape changing after translation

--- drafts/examples/broken_union.sketch.js
+++ drafts/examples/broken_union.sketch.js
@@ -11,8 +11,11 @@ function broken_union(sketch) {
 
     good.explode().translate(30, 30).stroke('orange'),
     good.translate(30, 0).fill('blue'),
+    good.translate(30, -30).rainbow(),
 
     bad.explode().translate(-30, 30).stroke('pink'),
+    bad.translate(-30, -30).rainbow(),
+    bad.rainbow().translate(-30, -60),
 
     bad.translate(-30, 0).fill('blue'),
     c.translate(-30, 0).fill('none').stroke('red'),
@@ -20,4 +23,13 @@ function broken_union(sketch) {
   );
   // Totally broken unions look like they dissapear because they have edges but no faces/
   // I think this is probably due to the edge order, winding, direction etc.
+
+  // Arc
+  // pc: { x: -30, y: -30}
+  // r: 10
+  // start: { x: -29, y: -39.95}
+  // end: { x: -40, y: -30}
+  // startAngle: 4.813
+  // endAngle: 3.142
+  // counterClockwise: true
 }
