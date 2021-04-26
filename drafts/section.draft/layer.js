753fa6e35a6ab60daab1ac804cf615aa863761ed
karimi <mortza@gmail.com>
2021-04-26 12:11:46 -0400

    Add style, width and padding to layer

--- drafts/section.draft/layer.js
+++ drafts/section.draft/layer.js
@@ -5,17 +5,41 @@ return {
     { name: "cavity", default: 30 },
     { name: "datum", default: null },
     { name: "material", default: "wood" },
+    { name: "infinite", default: true },
+    { name: "padding_bottom", default: 0 },
+    { name: "padding_top", default: 0 },
   ],
-  func: function (sketch, height, width, cavity, datum, material) {
-    const Colormap={wood: "yellow", steel:"blue", concrete:"grey", air: "transparent"}
-    const Layer = sketch.polycurve(
-      [cavity+width, 0],
-      [cavity, 0],
-      [cavity, -height],
-      [cavity+width, -height],
-    ).fill(Colormap[material]).stroke("transparent", 0)
-    
+  func: function (sketch, height, width, cavity, datum, material, infinite, padding_bottom, padding_top) {
+    const Style={
+      wood: {fill:"yellow", stroke:["black",.5]}, 
+      steel:{fill:"blue", stroke:["black",1]}, 
+      concrete:{fill:"grey", stroke:["black",2]}, 
+      air: {fill:"transparent", stroke:["transparent",0]}
+    }
+    if(infinite)
+    {
+      const Layer = sketch.polycurve(
+      [cavity+width, -padding_top],
+      [cavity, -padding_top],
+      [cavity, -height+padding_bottom],
+      [cavity+width, -height+padding_bottom],
+    ).fill(Style[material].fill)
+     .stroke(...Style[material].stroke)
     sketch.add(Layer);
+    }
+    else
+    {
+       const Layer = sketch.polycurve(
+      [cavity+width, -padding_top],
+      [cavity, -padding_top],
+      [cavity, -height+padding_bottom],
+      [cavity+width, -height+padding_bottom],
+      [cavity+width, -padding_top],
+    ).fill(Style[material].fill)
+     .stroke(...Style[material].stroke)
+    sketch.add(Layer);
+    }
+    
     
     if(datum){
       const Offset ={
