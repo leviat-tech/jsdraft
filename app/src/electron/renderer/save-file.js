f298703efd94c45033bc78343fb282b21b9c77fb
David Anderson <danderson@crh.io>
2021-04-26 18:39:51 -0400

    Allow file saving for nested folders

--- app/src/electron/renderer/save-file.js
+++ app/src/electron/renderer/save-file.js
@@ -2,20 +2,22 @@ const fs = require('fs');
 const path = require('path');
 
 
-async function saveFile(savePath, draft) {
-  const sketchFeatureDir = fs.existsSync(path.join(savePath, 'sketch-features'))
-    ? path.join(savePath, 'sketch-features')
-    : savePath;
+async function saveFile(savePath, files) {
+  const fileList = [];
+  const saveFiles = [];
+  fileList.push(...Object.entries(files).map((file) => ({ file, path: '' })));
 
-  const saveFiles = Object.entries(draft.features.sketch)
-    .map(([name, file]) => {
-      const filePath = path.join(sketchFeatureDir, `${name}.${file.extension}`);
-      return fs.promises.writeFile(filePath, file.contents);
-    })
-    .concat(fs.promises.writeFile(
-      path.join(savePath, 'index.json'),
-      JSON.stringify(draft.meta, null, 2),
-    ));
+  while (fileList.length > 0) {
+    const file = fileList.pop();
+    if (typeof file.file[1] === 'object') {
+      fileList.push(...Object.entries(file.file[1]).map((f) => ({ file: f, path: file.path + file.file[0] })));
+    } else {
+      saveFiles.push(fs.promises.writeFile(
+        path.join(savePath, file.path, file.file[0]),
+        file.file[1],
+      ));
+    }
+  }
 
   await Promise.all(saveFiles);
   return true;
--- app/src/utility/save-file-in-browser.js
+++ app/src/utility/save-file-in-browser.js
@@ -2,15 +2,23 @@ import JSZip from 'jszip';
 import downloadFile from './download-file.js';
 
 
-async function saveFileInBrowser(draftFilename, draft) {
+async function saveFileInBrowser(draftFilename, files) {
   const zip = new JSZip();
 
-  Object.entries(draft.features.sketch).forEach(([name, file]) => {
-    const filename = `sketch-features/${name}.${file.extension}`;
-    zip.file(filename, file.contents);
-  });
+  const fileList = [];
+  fileList.push(...Object.entries(files).map((file) => ({ file, path: '' })));
 
-  zip.file('index.json', JSON.stringify(draft.meta, null, 2));
+  while (fileList.length > 0) {
+    const file = fileList.pop();
+    if (typeof file.file[1] === 'object') {
+      fileList.push(...Object.entries(file.file[1]).map((f) => ({ file: f, path: file.path + file.file[0] })));
+    } else {
+      const path = file.path
+        ? `${file.path}/${file.file[0]}`
+        : file.file[0];
+      zip.file(path, file.file[1]);
+    }
+  }
 
   const content = await zip.generateAsync({ type: 'blob' });
   downloadFile(content, `${draftFilename}.draft.zip`);
