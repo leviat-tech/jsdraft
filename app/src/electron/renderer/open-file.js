ccadef57eedc000ecda151716ed5f374a120e7b8
David Anderson <danderson@crh.io>
2021-04-27 16:08:31 -0400

    Provide getFile electron function if path is known

--- app/src/electron/renderer/open-file.js
+++ app/src/electron/renderer/open-file.js
@@ -1,13 +1,6 @@
 const { ipcRenderer } = require('electron');
-const fs = require('fs');
 const path = require('path');
-const parseFilename = require('../utility/parse-filename.js');
-
-
-function isFile(p) {
-  const stat = fs.lstatSync(p);
-  return stat.isFile();
-}
+const getFile = require('./get-file.js');
 
 /*
 The imported file could be:
@@ -25,27 +18,12 @@ async function openFile() {
 
   const p = path.basename(d) === 'index.json' ? path.dirname(d) : d;
 
-  const sketchDir = fs.existsSync(path.join(p, 'sketch-features'))
-    ? path.join(p, 'sketch-features')
-    : p;
-
-  const sketchFeatureFiles = await fs.promises.readdir(sketchDir);
-
-  const files = sketchFeatureFiles
-    .filter((file) => isFile(path.join(sketchDir, file)))
-    .map((filename) => parseFilename(filename))
-    .filter((file) => file)
-    .map(async (file) => ({
-      ...file,
-      contents: await fs.promises.readFile(path.join(sketchDir, file.filename), 'utf-8'),
-    }));
+  const files = await getFile(p);
 
   return {
     filename: path.basename(directory.filePaths[0]),
     path: directory.filePaths[0],
-    files: {
-      sketch: await Promise.all(files),
-    },
+    files,
   };
 }
 
--- app/src/electron/renderer/preload.js
+++ app/src/electron/renderer/preload.js
@@ -1,5 +1,6 @@
 const { contextBridge } = require('electron');
 const openFile = require('./open-file.js');
+const getFile = require('./get-file.js');
 const saveAs = require('./save-as.js');
 const saveFile = require('./save-file.js');
 const watchDirectory = require('./watch-directory.js');
@@ -7,6 +8,7 @@ const watchDirectory = require('./watch-directory.js');
 
 contextBridge.exposeInMainWorld('electron', {
   openFile,
+  getFile,
   saveAs,
   saveFile,
   watchDirectory,
