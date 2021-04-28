a6619be33ca5830faa6bb8a8a979cbe5f752da4e
David Anderson <danderson@crh.io>
2021-04-28 11:50:27 -0400

    Add test for hidden behaviors

---
+++ test/features/meta/hide.spec.js
@@ -0,0 +1,37 @@
+/* global describe, it */
+
+const { expect, use } = require('chai');
+use(require('../../helpers.js'));
+const Sketch = require('../../../src/sketch/sketch.js');
+const render = require('../../../src/render.js');
+
+
+describe('Hide', () => {
+  const sketch = new Sketch()
+    .circle([0, 0], 50)
+    .hide();
+
+  const mixed = new Sketch()
+    .polyface([0, 0], [16, 16], [-10, 20], [-12, 2])
+    .add(sketch);
+
+  it('should omit hidden elements when rendering', () => {
+    const svg = render(mixed, 'svg', { viewport: 'js' });
+    expect(svg.length).to.eql(1);
+  });
+
+  it('should include hidden elements if indicated', () => {
+    const svg = render(mixed, 'svg', { viewport: 'js', show: 'all' });
+    expect(svg.length).to.eql(2);
+  });
+
+  it('should omit hidden entities from the entities list', () => {
+    const entities = render(mixed, 'entities');
+    expect(entities.length).to.eql(1);
+  });
+
+  it('should include hidden entities if indicated', () => {
+    const entities = render(mixed, 'entities', { show: 'all' });
+    expect(entities.length).to.eql(2);
+  });
+});
