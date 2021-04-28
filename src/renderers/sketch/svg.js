9da99556eea17779b0d73855ea201602a6a75b4a
David Anderson <danderson@crh.io>
2021-04-28 11:49:03 -0400

    Support 'show' property in svg renderer

--- src/renderers/sketch/svg.js
+++ src/renderers/sketch/svg.js
@@ -1,11 +1,23 @@
 const svg_renderer = require('../entity/svg');
 
 // Return an SVG string
-function recurse(sketch, style) {
+function recurse(sketch, style, show) {
   let svg = '';
 
+  if (sketch.node.hidden && show === 'visible') return svg;
+  if (!sketch.node.hidden && show === 'hidden') return svg;
+
   // set style
-  const s = { ...sketch.node.style, ...style };
+  let s;
+  if (sketch.node.hidden) {
+    s = {
+      stroke: { color: '#aaa', width: '1px' },
+      fill: { color: '#fff', opacity: '0.5' },
+      annotation: { color: '#fff' },
+    };
+  } else {
+    s = { ...sketch.node.style, ...style };
+  }
 
   // draw entities
   if (sketch.node.entity) {
@@ -14,35 +26,49 @@ function recurse(sketch, style) {
 
   // draw children
   for (const child of sketch.node.children) {
-    svg += recurse(child, s);
+    svg += recurse(child, s, show);
   }
 
   return svg;
 }
 
 // Return an array of JS objects respresenting SVG nodes
-function recurse_js(sketch, style) {
+function recurse_js(sketch, style, show) {
   let svg = [];
-  const s = { ...sketch.node.style, ...style };
+
+  if (sketch.node.hidden && show === 'visible') return svg;
+  if (!sketch.node.hidden && show === 'hidden') return svg;
+
+  let s;
+  if (sketch.node.hidden) {
+    s = {
+      stroke: { color: '#aaa', width: '1px' },
+      fill: { color: '#fff', opacity: '0.5' },
+      annotation: { color: '#fff' },
+    };
+  } else {
+    s = { ...sketch.node.style, ...style };
+  }
+
 
   if (sketch.node.entity) {
     svg = [...svg, svg_renderer(sketch.node.entity, { output: 'js', style: s })];
   }
 
   for (const child of sketch.node.children) {
-    svg = [...svg, ...recurse_js(child, s)];
+    svg = [...svg, ...recurse_js(child, s, show)];
   }
 
   return svg;
 }
 
-function render(sketch, { viewport = 'svg' } = {}) {
+function render(sketch, { viewport = 'svg', show = 'visible' } = {}) {
   if (viewport === null) {
-    return recurse(sketch, null);
+    return recurse(sketch, null, show);
   }
 
   if (viewport === 'js') {
-    return recurse_js(sketch, null);
+    return recurse_js(sketch, null, show);
   }
 
   return `<${viewport}>${recurse(sketch)}</${viewport}>`;
