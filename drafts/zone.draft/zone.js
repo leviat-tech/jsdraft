874be728c32b17caf1e0dffd735fa33438e9b663
karimi <mortza@gmail.com>
2021-04-29 15:01:52 -0400

    Zone segments angle

--- drafts/zone.draft/zone.js
+++ drafts/zone.draft/zone.js
@@ -1,15 +1,15 @@
 return {
   parameters: [
-    {name: "segments", default: [{length:100, angle:0}, {length:50, angle:20}, {length:50, angle:0}]},
+    {name: "segments", default: [{length:100, angle:0}, {length:50, angle:20}, {length:50, angle:-20}]},
   ],
   func: function (sketch, segments) {
     const sketches = [];
     let angle = 0;
-    let end =[0,0];
+    let next ={x:0,y:0};
     segments.forEach((segment, i) =>{
       angle+=segment.angle;
-      const Segment = sketch.user.segment(segment.length).rotate(angle).translate(...end)
-      end = [end[0]+segment.length*Math.cos(angle), end[1]+segment.length*Math.sin(angle)]
+      const Segment = sketch.user.segment(segment.length).rotate(angle).translate(next.x,next.y);
+      next = Segment.find("edge").vertices[1]
       sketches.push(Segment);
     })
     return sketch.add(...sketches);
