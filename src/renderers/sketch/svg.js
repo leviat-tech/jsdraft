2a415535c95dbb7274416cae29120f116e7c27b0
David Anderson <danderson@crh.io>
2021-04-29 17:41:38 -0400

    Allow greater control over SVG output

--- src/renderers/sketch/svg.js
+++ src/renderers/sketch/svg.js
@@ -1,4 +1,8 @@
+const get = require('lodash/get');
+const set = require('lodash/set');
 const svg_renderer = require('../entity/svg');
+const calculate_viewbox = require('../utility/viewbox.js');
+
 
 // Return an SVG string
 function recurse(sketch, style, show) {
@@ -62,16 +66,54 @@ function recurse_js(sketch, style, show) {
   return svg;
 }
 
-function render(sketch, { viewport = 'svg', show = 'visible' } = {}) {
+function render(sketch, {
+  viewport = 'svg',
+  show = 'visible',
+  fit = true,
+  aspect_ratio = 1,
+  padding = 0,
+  padding_top = 0,
+  padding_right = 0,
+  padding_bottom = 0,
+  padding_left = 0,
+  center = {},
+  size = 1000,
+  style = {},
+} = {}) {
   if (viewport === null) {
-    return recurse(sketch, null, show);
+    return recurse(sketch, style, show);
   }
 
   if (viewport === 'js') {
-    return recurse_js(sketch, null, show);
+    return recurse_js(sketch, style, show);
   }
 
-  return `<${viewport}>${recurse(sketch)}</${viewport}>`;
+  if (viewport === 'svg') {
+    const pad = {
+      top: padding_top || padding,
+      right: padding_right || padding,
+      bottom: padding_bottom || padding,
+      left: padding_left || padding,
+    };
+
+    const extents = sketch.extents;
+
+    const scale = get(style, 'annotation.scale') || 1;
+
+    const { viewbox, scalefactor } = calculate_viewbox(
+      extents, fit, pad, aspect_ratio, scale, center, size,
+    );
+
+
+    set(style, 'annotation.scale', scale * scalefactor);
+
+    return `<${viewport} viewBox="${viewbox}" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" display="block">
+  <g transform="scale(1 -1)">${recurse(sketch, style, show)}
+  </g>
+</${viewport}>`;
+  }
+
+  return `<${viewport}>${recurse(sketch, style, show)}</${viewport}>`;
 }
 
 
---
+++ test/render.spec.js
@@ -0,0 +1,49 @@
+/* global describe, it */
+
+const { expect, use } = require('chai');
+const { parse } = require('svg-parser');
+use(require('./helpers.js'));
+const Sketch = require('../src/sketch/sketch.js');
+const render = require('../src/render.js');
+
+
+describe('Render', () => {
+  const sketch = new Sketch()
+    .polyface([0, 0], [16, 16], [-10, 20], [-12, 2])
+    .aligned_dim([-12, 2], [0, 0]);
+
+
+  it('should render an svg string', () => {
+    const svg = render(sketch, 'svg');
+    const parsed = parse(svg);
+    expect(parsed.children[0].properties.viewBox).to.eql('-12 -24 28 28');
+  });
+
+  it('should allow padding', () => {
+    const svg = render(sketch, 'svg', { padding: 1 });
+    const parsed = parse(svg);
+    expect(parsed.children[0].properties.viewBox).to.eql('-13 -25 30 30');
+  });
+
+  it('should plot to scale', () => {
+    const svg = render(sketch, 'svg', { fit: false, size: 50 });
+    const parsed = parse(svg);
+    const svgnode = parsed.children[0];
+    const textnode = parsed.children[0].children[0].children[1].children[1];
+    expect(textnode.properties['font-size'].toFixed(3)).to.eql('0.600');
+    expect(svgnode.properties.viewBox).to.eql('-23 -35 50 50');
+  });
+
+  it('should allow annotations to scale', () => {
+    const svg = render(sketch, 'svg', { style: { annotation: { scale: 2 } } });
+    const parsed = parse(svg);
+    const textnode = parsed.children[0].children[0].children[1].children[1];
+    expect(textnode.properties['font-size'].toFixed(3)).to.eql('0.672');
+  });
+
+  it('can use an aspect ratio box', () => {
+    const svg = render(sketch, 'svg', { aspect_ratio: 2 });
+    const parsed = parse(svg);
+    expect(parsed.children[0].properties.viewBox).to.eql('-18 -20 40 20');
+  });
+});
