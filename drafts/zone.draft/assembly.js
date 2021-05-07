621f13a022c5727cf8cf0d801702462338772f82
karimi <mortza@gmail.com>
2021-05-07 16:30:01 -0400

    Fix cut condition for edges

--- drafts/zone.draft/assembly.js
+++ drafts/zone.draft/assembly.js
@@ -3,7 +3,7 @@ return {
     {name: "length", default: 100},
     {name: "width", default: 10},
     {name: "left", default: {type: "cut"}},
-    {name: "right", default: null},
+    {name: "right", default: {type: "cut"}},
   ],
   
   func: function (sketch, length, width, left, right) {
@@ -13,14 +13,14 @@ return {
         const Flip = {right: -1, left: 1};
         const Left = base.hidden.find("left");
         const Right = base.hidden.find("right");
-        const Edge = base.hidden.find(side);
+        const Edge = base.hidden.find(side).show();
         const FarEdge= Edge.offset(Flip[side]*10);
         const Triangle = sketch.polyface(
           ...Edge.vertices,
           FarEdge.vertices[0]
         )
-        const Union = base.union(Triangle);
-        return sketch.new.add(Union, Left.hide().name("left"), Right.hide().name("right"));
+//         const Union = base.union(Triangle);
+        return sketch.new.add(base, Triangle);
       }
     };
     const Rect = sketch.rectangle(0,0,length, width);
@@ -33,7 +33,7 @@ return {
     {Assembly = Modifiers[left.type](Assembly, "left",left);}
     if(right)
     {
-//     Assembly = Modifiers[right.type](Assembly, "right",right);
+    Assembly = Modifiers[right.type](Assembly, "right",right);
     }
 
     sketches.push(Assembly);
