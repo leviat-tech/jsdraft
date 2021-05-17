6b350b0022f8d077e6892a6ca03d1535a7a1cbf0
karimi <mortza@gmail.com>
2021-05-17 15:28:03 -0400

    Place pressureplare relative to wp

--- drafts/bracket.draft/bracket.js
+++ drafts/bracket.draft/bracket.js
@@ -5,17 +5,24 @@ return {
   func: function (sketch, wv) {
     const wp = sketch.user.wp()
     let lower = {x:Infinity, y: Infinity}
+    let upper = {x:-Infinity, y: -Infinity}
     wp.vertices.forEach(v =>{ 
       if (v.x<lower.x){
       lower.x = v.x
       }
       if (v.y<lower.y){
       lower.y = v.y
-      }                     
+      }
+      if (v.x>upper.x){
+      upper.x = v.x
+      }
+      if (v.y>upper.y){
+      upper.y = v.y
+      } 
     })
     const angle = sketch.user.angle().translate(lower.x, lower.y-wv)
-    const adjustmentplate = sketch.user.adjustmentplate()
-    const pressureplate = sketch.user.pressureplate()
+    const adjustmentplate = sketch.user.adjustmentplate().translate(upper.x, upper.y)
+    const pressureplate = sketch.user.pressureplate().translate(upper.x, upper.y)
     return sketch.add(angle,wp, adjustmentplate, pressureplate)
   }
 }
--- drafts/bracket.draft/pressureplate.js
+++ drafts/bracket.draft/pressureplate.js
@@ -1,6 +1,6 @@
 return {
   parameters: [
-    { name: "height", default: 50 },
+    { name: "height", default: 45 },
     { name: "width", default: 10 },
     { name: "thickness", default: 5 },
     { name: "radius", default: 5 }
@@ -21,7 +21,7 @@ return {
       outer.vertices.slice(-1)[0],
       )
     const angle = sketch.new.add(outer, inner, top, bottom)
-    .join()
+    .join().translate(thickness, 0)
     
     return sketch.add(angle)
   }
