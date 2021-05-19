dc05911582c05cea522b77a0780da1df1ef802bf
karimi <mortza@gmail.com>
2021-05-19 12:07:26 -0400

    Add hole to adjustmentplate

--- drafts/bracket.draft/adjustmentplate_front.js
+++ drafts/bracket.draft/adjustmentplate_front.js
@@ -1,40 +1,62 @@
 return {
   parameters: [
-    { name: "params", default: 
     {
-    height: 70 ,
-    depth: 40 ,
-    width: 15,
-    thickness: 5,
-    overhang: {height: 5, width: 3} ,
-    cutout: {height: 5,width: 3} ,
-    teeth: {height:5, depth: 5, positions:[0, 10, 20]},
-    }
-  }
+      name: "params",
+      default: {
+        height: 70,
+        depth: 40,
+        width: 20,
+        thickness: 5,
+        overhang: { height: 5, width: 3 },
+        hole: { x: 10, y: 10, d: 5 },
+        cutout: { height: 5, width: 3 },
+        teeth: {
+          height: 5,
+          depth: 5,
+          positions: [0, 10, 20],
+        },
+      },
+    },
   ],
   func: function (sketch, params) {
-   const inner = sketch
-  .polycurve(
-    [params.width/2, params.height],
-    [params.width/2, params.width/2+params.thickness],
-    -1, 
-    [-params.width/2, params.width/2+params.thickness],
-    [-params.width/2, params.height]
-  )
-  
-    const outer = inner.offset(params.thickness);
-    
+    const outer = sketch.polycurve(
+      [params.width / 2, params.height],
+      [params.width / 2, params.width / 2],
+      -1,
+      [-params.width / 2, params.width / 2],
+      [-params.width / 2, params.height]
+    );
+
+    const inner = outer.offset(-params.thickness);
+
     const left = sketch.polycurve(
       outer.vertices[0],
-      inner.vertices[0],
-      )
+      inner.vertices[0]
+    );
     const right = sketch.polycurve(
       inner.vertices.slice(-1)[0],
-      outer.vertices.slice(-1)[0],
-      )
-    const U = sketch.new.add(outer, inner, left, right).join()
+      outer.vertices.slice(-1)[0]
+    );
+    const U = sketch.new
+      .add(outer, inner, left, right)
+      .join();
 
-    return sketch.add(U)
-  }
-}
+    const hole = sketch.arc(
+      [
+        -params.width / 2 + params.thickness,
+        params.height - params.hole.y - params.hole.d,
+      ],
+      1,
+      [
+        -params.width / 2 + params.thickness,
+        params.height - params.hole.y,
+      ]
+    );
 
+    const holes = [1, -1].map((dir) => {
+      return hole.scale(dir, 1);
+    });
+
+    return sketch.add(U, ...holes);
+  },
+};
--- drafts/bracket.draft/adjustmentplate_side.js
+++ drafts/bracket.draft/adjustmentplate_side.js
@@ -1,38 +1,58 @@
 return {
   parameters: [
-    { name: "params", default: 
     {
-    height: 70 ,
-    depth: 40 ,
-    width: 15,
-    overhang: {height: 5, depth: 3} ,
-    cutout: {height: 5,depth: 3} ,
-    teeth: {height:5, depth: 5, positions:[0, 10, 20]},
-    }
-  }
+      name: "params",
+      default: {
+        height: 70,
+        depth: 40,
+        width: 15,
+        overhang: { height: 5, depth: 3 },
+        hole: { x: 10, y: 10, d: 5 },
+        cutout: { height: 5, depth: 3 },
+        teeth: {
+          height: 5,
+          depth: 5,
+          positions: [0, 10, 20],
+        },
+      },
+    },
   ],
   func: function (sketch, params) {
-   const back = sketch
-  .polycurve(
-    [0, 0],
-    [0,-params.teeth.depth],
-    [params.teeth.height,-params.teeth.depth],
-    [params.teeth.height,0]
-  )
-  .interpolate([0, 0], [0, -30], params.teeth.positions).translate(0,params.height-params.overhang.height);
+    const back = sketch
+      .polycurve(
+        [0, 0],
+        [0, -params.teeth.depth],
+        [params.teeth.height, -params.teeth.depth],
+        [params.teeth.height, 0]
+      )
+      .interpolate([0, 0], [0, -30], params.teeth.positions)
+      .translate(0, params.height - params.overhang.height);
 
-  const plate = sketch.polyface(
-    [0,0],
-    [-params.depth, 0],
-    [-params.depth, params.height-params.cutout.height],
-    [-params.depth+params.cutout.depth, params.height-params.cutout.height],
-    [-params.depth+params.cutout.depth, params.height],
-    [params.overhang.depth,params.height],
-    [params.overhang.depth,params.height-params.overhang.height],
-    ...back.vertices,
-    )
+    const plate = sketch.polyface(
+      [0, 0],
+      [-params.depth, 0],
+      [-params.depth, params.height - params.cutout.height],
+      [
+        -params.depth + params.cutout.depth,
+        params.height - params.cutout.height,
+      ],
+      [-params.depth + params.cutout.depth, params.height],
+      [params.overhang.depth, params.height],
+      [
+        params.overhang.depth,
+        params.height - params.overhang.height,
+      ],
+      ...back.vertices
+    );
 
-    return sketch.add(plate)
-  }
-}
+    const hole = sketch.circle(
+      [
+        -params.depth + params.hole.x,
+        params.height - params.hole.y,
+      ],
+      params.hole.d / 2
+    );
 
+    return sketch.add(plate.subtract(hole));
+  },
+};
