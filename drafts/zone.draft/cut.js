cf1d4961b9e2387edf1b53eceaf858dab45ff430
karimi <mortza@gmail.com>
2021-07-29 15:26:39 -0400

    Remove product related drafts

--- drafts/zone.draft/cut.js
+++
@@ -1,8 +0,0 @@
-function cut(sketch, side = 'left') {
-  const dir = side === 'left' ? 1 : -1;
-  const offset = sketch.offset(dir * 10);
-  return sketch.new.polyface(
-    offset.vertices[0],
-    ...sketch.vertices,
-  );
-}
